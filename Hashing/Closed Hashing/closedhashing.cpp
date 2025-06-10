efa0d68b2716a741b85b66f1d93b76f616164477
frager_098 <hafizmuhammadarham042@gmail.com>
2025-06-10 07:13:17 +0500

    Optimize Closed Hashing

--- Hashing/Closed Hashing/closedhashing.cpp
+++ Hashing/Closed Hashing/closedhashing.cpp
@@ -6,85 +6,80 @@ void insert(int size , int *arr){
     cin>>key;
     int ind;
     ind = key % (size ) ;
-
     if(arr[ind] == -1 ){
         arr[ind] = key ;
     }
     else{
-        int pro=10;
-            cout<<"You want to do linear probabbing or quadratic???"<<endl;
-            cout<<"Press 1 for linear and 2 for quadratic and 0 to exit"<<endl;
-            cin>>pro;
-            if(pro==1){
-                while(arr[ind] != -1){
-                    ind++;
-                    if(ind >= size){
-                    ind = 0 ;
-                    break;
+        int pro;
+        cout<<"You want to do linear probing or quadratic???"<<endl;
+        cout<<"Press 1 for linear and 2 for quadratic and 0 to exit"<<endl;
+        cin>>pro;
+        if(pro==1){
+            int start = ind;
+            while(arr[ind] != -1){
+                ind = (ind + 1) % size;
+                if(ind == start){
+                    cout<<"Hash Table is Full"<<endl;
+                    return;
                 }
-                if(arr[ind]== -1 ){
-                    arr[ind] = key ;
-                    break;
-                }
-                }
+        }
+            arr[ind] = key ;
+        }
+        else if(pro==2){
+            int n =1;
+            int quad = ind;
+            int attempts = 0;
+            while(arr[quad] != -1 && attempts < size  ){
+                quad = ( ind + (n*n) ) % size ;
+                n++ ;
+                attempts++ ;
             }
-            else if(pro==2){
-                int n =1;
-                int quad = ind;
-                int stop =0;
-                while(arr[quad] != -1 && stop != 3  ){
-                    quad = ( ind + (n*n) ) % size ;
-                    n++ ;
-                    if(arr[quad] == -1) {
-                        arr[quad] = key ;
-                        break;
-                    }
-                    if( ind == quad  ){
-                        stop++ ;
-                        cout<<stop;
-                    }
-                    if(stop==3){
-                        cout<<"Not possible with quadratic probabing now"<<endl<<"collision at "<<ind<<" index."<<endl;
-                    }
-                    
-                }
-                   
+            if(arr[quad] == -1) {
+                arr[quad] = key ;
+                
             }
             else{
-                cout<<"exit";
-            }
+                cout<<"Not possible with quadratic probabing now"<<endl<<"collision at "<<ind<<" index."<<endl;
+            }                   
+                
+        }
+        else{
+            cout<<"exit";
+        }
     }
 }
 
 void  print(int size ,int *arr){
-
-        for(int i=0;i<size;i++){
-            cout<<arr[i]<<"   ";
-        }cout<<endl;
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<"   ";
     }
+    cout<<endl;
+}
 int main(){
     int size;
     cout<<"Enter size of an array"<<endl;
     cin>>size;
-    int arr[size];
-    for(int i=0;i<=size;i++){
+    // int arr[size];
+    int *arr = new int[size];
+    for(int i=0;i<size;i++){
         arr[i] = -1;
-    }cout<<endl;
-
-    int inp=10;
-    while(inp!=0){
+    }
+    int inp;
+    while(true){
     cout<<"Enter 0 to exit"<<endl;
-    cout<<"Enter 1 to print"<<endl;
-    cout<<"Enter 2 to insert value";
+    cout<<"Enter 1 to insert value"<<endl;
+    cout<<"Enter 2 to print"<<endl;
     cin>>inp;
         if(inp==0){
-            cout<<"exit"<<endl;
+            cout<<"Exit!"<<endl;
+            delete(arr);
+            return 0;
         }
         if(inp==1){    
-            print(size , arr);
+            insert(size , arr);
         }
         if(inp==2){
-            insert(size , arr);
+            print(size , arr);
         }
     }
 }
