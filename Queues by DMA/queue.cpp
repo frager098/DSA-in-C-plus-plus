b797d3ba02262ab77412c1e9c35eee3c40c6cae0
frager_098 <hafizmuhammadarham042@gmail.com>
2025-06-10 05:49:12 +0500

    Optimize Queue

--- Queues by DMA/queue.cpp
+++ Queues by DMA/queue.cpp
@@ -4,37 +4,38 @@ using namespace std;
 struct queue{
     int num;
     queue* next;
+    queue(int n):num(n),next(NULL){}
 };
 void enQueue(queue** front,queue** rear){
-    queue* ptr=(queue*)malloc(sizeof(queue));
     int num;
-    cout<<"Enter value: ";
+    cout<<"Enter data: ";
     cin>>num;
-    // queue* curr=*front;
-    ptr->num=num;
-    ptr->next=NULL;
+    queue * ptr = new queue(num);
     if(*front==NULL && *rear==NULL){
         *front=ptr;
         *rear=*front;
     }
     else{
-        while((*rear)->next!=NULL){
-            *rear=(*rear)->next;
-            //  cout<<"ok";
-            
-        }
-        (*rear)->next=ptr;
-        *rear=(*rear)->next;
+        (*rear) -> next = ptr;
+        (*rear) = ptr;
     }
 }
 void deQueue(queue** front,queue** rear){
-        cout<<"g";
-    queue* curr=(*front);
-    (*front)=curr->next;
-    free(curr);
-        cout<<"hi";
+    if(*front==NULL){
+        cout<<"Queue is empty!"<<endl;
+    }
+    else if(*front == *rear){
+        delete(*front);
+        *front = NULL;
+        *rear = NULL ;
+    }
+    else{
+        queue* curr=(*front);
+        (*front)=curr->next;
+        delete(curr);
+    }
 }
-void print(queue** front,queue** rear){
+void print(queue** front){
     if(*front==NULL){
         cout<<"Queue is empty";
     }
@@ -43,32 +44,35 @@ void print(queue** front,queue** rear){
     while(curr!=NULL){
         cout<<"Queue: "<<curr->num<<"--->";
         curr=curr->next;
-        // if(*rear=)
-    }
+        }
     }
     cout<<endl;
 }
 int main(){
-    int i =0;
+    int i;
     queue* front=NULL;
     queue* rear=NULL;
-    while(i!=4){
+    while(true){
+        cout<<"Press 0 to exit"<<endl;
         cout<<"Press 1 to add value in a Queue: "<<endl;
         cout<<"Press 2 to delete value in a Queue: "<<endl;
         cout<<"Press 3 to print a Queue: "<<endl;
-        cout<<"Press 4 to exit"<<endl;
         cin>>i;
-        if(i==1){
+        if(i == 0 ){
+            cout<<"Exit!";
+            return 0 ;
+        }
+        else if(i==1){
             enQueue(&front,&rear);
         }
         else if(i==2){
             deQueue(&front,&rear);
         }
         else if(i==3){
-            print(&front,&rear);
+            print(&front);
         }
-    }
-    if(i==4){
-        cout<<"exit";
+        else{
+            cout<<"Wrong Input,Try Again!"<<endl;
+        }
     }
 }
--- Stacks/Stack by DMA/stack.cpp
+++ Stacks/Stack by DMA/stack.cpp
@@ -6,18 +6,18 @@ struct Stack{
     Stack* next;
     Stack(int n):num(n),next(NULL){}
 };
-void push(Stack** top){
+void push(Stack* top){
     cout<<"Enter Number:";
     int num;
     cin>>num;
     Stack* ptr = new Stack(num);
-    if(*top == NULL){
-        *top = ptr;
+    if(top == NULL){
+        top = ptr;
         cout<<"NULL";
     }
     else{
-        ptr->next = *top;
-        *top = ptr;
+        ptr->next = top;
+        top = ptr;
     }
 }
 
@@ -30,13 +30,19 @@ void pop(Stack** top){
     *top=curr->next;
     delete(curr);
     }
-
-void print(Stack** top){
-    if(*top == NULL){
+void peek(Stack *top){
+    if(top == NULL){
         cout<<"Stack is Empty!"<<endl;
         return;
     }
-    Stack* curr=*top;
+    cout<<top->num<<endl;
+}
+void print(Stack* top){
+    if(top == NULL){
+        cout<<"Stack is Empty!"<<endl;
+        return;
+    }
+    Stack* curr=top;
     while(curr!=NULL){
         cout<<"Stack: "<<curr->num<<"--->";
         curr=curr->next;
@@ -51,6 +57,7 @@ int main(){
         cout<<"Press 1 to push data in stack: "<<endl;
         cout<<"Press 2 to pop data in a stack: "<<endl;
         cout<<"Press 3 to print data in a stack: "<<endl;
+        cout<<"Press 4 to print data from top of the stack: "<<endl;
         cout<<"Enter:";
         cin>>i;
         if(i == 0 ){
@@ -58,13 +65,16 @@ int main(){
             return 0 ;
         }
         else if(i==1){
-            push(&top);
+            push(top);
         }
         else if(i==2){
             pop(&top);
         }
         else if(i==3){
-          print(&top);
+          print(top);
+        }
+        else if(i==4){
+          peek(top);
         }
         else{
             cout<<"Wrong Input,Try Again!"<<endl;
