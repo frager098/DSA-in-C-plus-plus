1041c924ddd8f3baab3f32e5ec1d13f531fda766
frager_098 <hafizmuhammadarham042@gmail.com>
2025-06-15 00:37:47 +0500

    Update Bubble Sort

--- Sorting Algorithms/Bubble Sort/bubbleSort.cpp
+++ Sorting Algorithms/Bubble Sort/bubbleSort.cpp
@@ -1,23 +1,63 @@
 #include <iostream>
 using namespace std;
-
-int main(){
-  int arr[] = {6,10,2,1,0,3,5,100,30,55,-3};
-  int size = sizeof(arr)/sizeof(arr[0]);
-  for ( int i = 0 ; i  < size - 1 ; i++ ) //loop to iterate an array size - 1 number of times
-  {
-    for ( int j = 0 ; j < size - 1  ; j++) // loop to compare adjacent elements 
-    {
-      if(arr[j]>arr[j+1])
-      {
-        int temp = arr[j+1];
-        arr[j+1]=arr[j];
-        arr[j] = temp ;
-      }
+void insert(int * arr , int size , int num){
+    for(int i = 0 ; i < size ; i++ ){
+        if(arr[i] == -1){
+            arr[i] = num;
+            return;
+        }
     }
-  }
-  for ( int i = 0 ; i < size ; i++)
-  {
-    cout<<arr[i]<<endl;
-  }
+}
+void bubbleSort(int * arr ,int size){
+  for(int i = 0 ; i < size  ; i++){
+        for(int j = 0 ; j < size - i - 1 ; j++){
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        
+  }  
+}
+
+void print(int * arr,int size){
+    cout<<"[";
+    for(int i = 0 ; i < size ; i++ ){
+        cout<<arr[i]<<" ";
+    } cout<<"]"<<endl;
+}
+int main(){
+    int size , num , inp;
+    cout<<"Enter Size of An Array:";
+    cin>>size;
+    int * arr = new int[size];
+    for(int i = 0 ; i < size ; i++ ){
+        arr[i] = -1 ;
+    } 
+   while(true){
+    cout<<"Press 0 to Exit"<<endl;
+    cout<<"Press 1 to Insert Number in an Array"<<endl;
+    cout<<"Press 3 to Sort An array using Bubble Sort"<<endl;
+    cout<<"Press 4 to print an Array"<<endl;
+    cin>>inp;
+    if(inp == 0){
+        cout<<"Exit"<<endl;
+        return 0;
+    }
+    else if(inp == 1){
+        cout<<"Enter Number You want to Insert:";
+        cin>>num;
+        insert(arr,size,num);
+    }
+    else if(inp == 3 ){
+        bubbleSort(arr,size);
+    }
+    else if(inp == 4 ){
+        print(arr,size);
+    }
+    else{
+        cout<<"Wrong Input,Try Again!"<<endl;
+    }
+   }
 }
