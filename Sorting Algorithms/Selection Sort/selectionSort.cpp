71a2ee293e6b729e80dec756ec44ebfea1a55472
frager_098 <hafizmuhammadarham042@gmail.com>
2025-06-15 04:20:52 +0500

    Update Selection  Sort

--- Sorting Algorithms/Selection Sort/selectionSort.cpp
+++ Sorting Algorithms/Selection Sort/selectionSort.cpp
@@ -1,25 +1,62 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-  int arr[] = {3, 2, 1,5,40,-2,-43};
-  int size = sizeof(arr)/sizeof(arr[0]);
-  for (int i = 0; i < size; i++) //loop to hold each element one by one to swap it with min or max value in array
-  {
-    int min_index = i; //Variable to hold index of minimum value in array
-    for (int j = i + 1; j < size; j++)//loop for comparing holded value with the whole array 
-    {
-      if (arr[min_index] > arr[j])
-      {
-        min_index = j;
-      }
+void insert(int * arr , int size , int num){
+    for(int i = 0 ; i < size ; i++ ){
+        if(arr[i] == -1){
+            arr[i] = num;
+            return;
+        }
     }
-    int min = arr[min_index]; // local variable for swapping
-    arr[min_index] = arr[i];
-    arr[i] = min ;
-  }
-  for (int i = 0; i < size; i++)
-  {
-    cout << arr[i] << endl;
-  }
+}
+void selectionSort(int * arr ,int size){
+  for(int i = 0 ; i < size  ; i++){
+    int min =  i;
+        for(int j = i ; j < size - 1 ; j++){
+          if(arr[min] > arr[j+1]) min = j+1;          
+          }
+          int temp = arr[i];
+          arr[i] = arr[min];
+          arr[min] = temp ;
+  }  
+}
+
+void print(int * arr,int size){
+    cout<<"[";
+    for(int i = 0 ; i < size ; i++ ){
+        cout<<arr[i]<<" ";
+    } cout<<"]"<<endl;
+}
+int main(){
+    int size , num , inp;
+    cout<<"Enter Size of An Array:";
+    cin>>size;
+    int * arr = new int[size];
+    for(int i = 0 ; i < size ; i++ ){
+        arr[i] = -1 ;
+    } 
+   while(true){
+    cout<<"Press 0 to Exit"<<endl;
+    cout<<"Press 1 to Insert Number in an Array"<<endl;
+    cout<<"Press 3 to Sort An array using Bubble Sort"<<endl;
+    cout<<"Press 4 to print an Array"<<endl;
+    cin>>inp;
+    if(inp == 0){
+        cout<<"Exit"<<endl;
+        return 0;
+    }
+    else if(inp == 1){
+        cout<<"Enter Number You want to Insert:";
+        cin>>num;
+        insert(arr,size,num);
+    }
+    else if(inp == 3 ){
+        selectionSort(arr,size);
+    }
+    else if(inp == 4 ){
+        print(arr,size);
+    }
+    else{
+        cout<<"Wrong Input,Try Again!"<<endl;
+    }
+   }
 }
